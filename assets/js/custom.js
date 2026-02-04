1635060836104b97c100d4c6a8fc96c51ff6e836
SahilGite4fox <sahilgite.4fox@gmail.com>
2026-02-04 10:06:13 +0530

    minor changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -210,7 +210,7 @@ $(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 6000,
     infinite: true,
     arrows: false,
     fade: true,
